f236fb47357c2d263803d69405358e92dd4332bf
Outsider <outsideris@gmail.com>
2021-02-03 02:42:11 +0900

    kubebuilder create api
    Signed-off-by: Outsider <outsideris@gmail.com>

--- cnat-kubebuilder/main.go
+++ cnat-kubebuilder/main.go
@@ -23,9 +23,11 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	cnatv1alpha1 "k8s.io/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/controllers"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -37,6 +39,7 @@ var (
 func init() {
 	_ = clientgoscheme.AddToScheme(scheme)
 
+	_ = cnatv1alpha1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -63,6 +66,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.AtReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("At"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "At")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
